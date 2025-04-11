2ff2de045a7582d628aec433fe58829e3e961c88
Vignesh Rajappa <vigneshppa@gmail.com>
2025-04-11 13:30:10 +0530

    created needed model files

--- src/main/java/com/api/carrental/model/BuyerHistory.java
+++ src/main/java/com/api/carrental/model/BuyerHistory.java
@@ -1,28 +1,23 @@
 package com.api.carrental.model;
 
-import java.time.LocalDate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Buyer {
+public class BuyerHistory {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int buyer_id;
+	
 	@ManyToOne
-    @JoinColumn(name = "car_id")
-    private Car car;
-
+    private Buyer buyer;
+    
     @ManyToOne
-    @JoinColumn(name = "user_id")
     private User user;
-
-    private LocalDate DateofBuying;
 
 	public int getBuyer_id() {
 		return buyer_id;
@@ -32,12 +27,12 @@ public class Buyer {
 		this.buyer_id = buyer_id;
 	}
 
-	public Car getCar() {
-		return car;
+	public Buyer getBuyer() {
+		return buyer;
 	}
 
-	public void setCar(Car car) {
-		this.car = car;
+	public void setBuyer(Buyer buyer) {
+		this.buyer = buyer;
 	}
 
 	public User getUser() {
@@ -46,14 +41,6 @@ public class Buyer {
 
 	public void setUser(User user) {
 		this.user = user;
-	}
-
-	public LocalDate getDateofBuying() {
-		return DateofBuying;
-	}
-
-	public void setDateofBuying(LocalDate dateofBuying) {
-		DateofBuying = dateofBuying;
 	}
     
     
